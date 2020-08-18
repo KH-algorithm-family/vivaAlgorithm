764f982adc24d1ff3a06596c41510d27539b1758
minjung <alswjddl404@gmail.com>
2020-08-18 12:35:43 +0900

    2020-08-18_12:35 add test1

---
+++ algorithmProject/src/algo/kmj/test1.java
@@ -0,0 +1,17 @@
+package algo.kmj;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		int result = 0;
+		
+		for(int i = 0; i < 1000; i++) {
+			if(i % 3 == 0 || i % 5 == 0) {
+				result += i;
+			}
+		}
+		System.out.println("결과 : " + result);
+	}
+
+}
